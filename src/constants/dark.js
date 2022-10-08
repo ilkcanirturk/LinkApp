8ad0377d7e665db9e1c71cffc3303fb742836029
ilkcan irturk <ilkcan.irturk@outlook.com>
2022-10-09 00:36:47 +0300

    add constants

---
+++ src/constants/dark.js
@@ -0,0 +1,16 @@
+export default {
+    type: 'dark',
+    backgroundColor: '#1b1b1b',
+    textColor: 'white',
+    textInfoColor: '#212121',
+    headerBgColor: '#424242',
+    headerShadowColor: "#5a9bc9",
+    tabBarBgColor: '#424242',
+    tabBarOnlineColor: '#5a9bc9',
+    tabBarOnlineTintColor: '#5a9bc9',
+    tabBarOfflineColor: 'gray',
+    borderColor: '#e5ffff',
+    boxColor: '#e5ffff',
+    chatIconColor:'white',
+    iconColor: '#5a9bc9'
+  };
---
+++ src/constants/light.js
@@ -0,0 +1,16 @@
+export default {
+    type: 'light',
+    backgroundColor: 'white',
+    textColor: 'black',
+    textInfoColor: '#212121',
+    headerBgColor: '#e5ffff',
+    headerShadowColor: "#5a9bc9",
+    tabBarBgColor: '#e5ffff',
+    tabBarOnlineColor: '#5a9bc9',
+    tabBarOnlineTintColor: '#5a9bc9',
+    tabBarOfflineColor: 'gray',
+    borderColor: '#5a9bc9',
+    boxColor: '#e5ffff',
+    chatIconColor:'white',
+    iconColor: '#5a9bc9'
+  };
