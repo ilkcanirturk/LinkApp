250d969f6bbd063d485a99248b3d7846db3721e4
ilkcan irturk <ilkcan.irturk@outlook.com>
2022-10-09 00:37:50 +0300

    add Data file

---
+++ src/data/ContactDataList.js
@@ -0,0 +1,56 @@
+export const contactDataList=[
+    {
+        id:'a1',
+        firstName:'Emre',
+        ppURL:'https://picsum.photos/id/152/200/300',
+        phoneNumber:'(505) 607 2685'
+    },
+    {
+        id:'a2',
+        firstName:'Gamze',
+        ppURL:'https://picsum.photos/id/145/200/300',
+        phoneNumber:'(505) 627 1145'
+    },
+    {
+        id:'a3',
+        firstName:'Ali',
+        ppURL:'https://picsum.photos/id/141/200/300',
+        phoneNumber:'(536) 295 2190'
+    },
+    {
+        id:'a4',
+        firstName:'Gökay',
+        ppURL:'https://picsum.photos/id/142/200/300',
+        phoneNumber:'(507) 433 2395'
+    },
+    {
+        id:'a5',
+        firstName:'Derya',
+        ppURL:'https://picsum.photos/id/111/200/300',
+        phoneNumber:'(507) 667 2807'
+    },
+    {
+        id:'a6',
+        firstName:'Onur',
+        ppURL:'https://picsum.photos/id/135/200/300',
+        phoneNumber:'(536) 782 5837'
+    },
+    {
+        id:'a7',
+        firstName:'Mehmet',
+        ppURL:'https://picsum.photos/id/110/200/300',
+        phoneNumber:'(506) 256 5891'
+    },
+    {
+        id:'a8',
+        firstName:'Ezgi',
+        ppURL:'https://picsum.photos/id/191/200/300',
+        phoneNumber:'(536) 789 4554'
+    },
+    {
+        id:'a9',
+        firstName:'Soner',
+        ppURL:'https://picsum.photos/id/225/200/300',
+        phoneNumber:'(536) 732 5562'
+    }
+]
